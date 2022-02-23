eb1c61ad2f4f24a201ceb3402f6740952fa192d1
biggicode <larioncescu@yahoo.com>
2022-02-23 14:37:08 +0200

    add callback function

--- src/components/accordion/accordion.component.jsx
+++ src/components/accordion/accordion.component.jsx
@@ -1,18 +1,22 @@
 import React from "react"
 
 export const Accordion = ({ items }) => {
+  const handleTitleClick = (index) => {
+    console.log("clicked", index)
+  }
+
   return (
     <div className="ui styled accordion">
-      {items.map((item) => (
-        <div key={item.title}>
-          <div className="title active">
+      {items.map((item, index) => (
+        <React.Fragment key={item.title}>
+          <div className="title active" onClick={() => handleTitleClick(index)}>
             <i className="dropdown icon"></i>
             {item.title}
           </div>
           <div className="content active">
             <p>{item.content}</p>
           </div>
-        </div>
+        </React.Fragment>
       ))}
     </div>
   )
