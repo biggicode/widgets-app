08159c099e7153125c9a3c18228706a0d862946d
biggicode <larioncescu@yahoo.com>
2022-02-23 14:26:00 +0200

    add content to accordion and style it

--- src/components/accordion/accordion.component.jsx
+++ src/components/accordion/accordion.component.jsx
@@ -1,5 +1,19 @@
 import React from "react"
 
 export const Accordion = ({ items }) => {
-  return <div>{items.length}</div>
+  return (
+    <div className="ui styled accordion">
+      {items.map((item) => (
+        <div key={item.title}>
+          <div className="title active">
+            <i className="dropdown icon"></i>
+            {item.title}
+          </div>
+          <div className="content active">
+            <p>{item.content}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
