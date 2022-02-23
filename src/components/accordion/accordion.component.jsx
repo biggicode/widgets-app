14c5490e945572e64eca47dfba3f9733d9cf0c08
biggicode <larioncescu@yahoo.com>
2022-02-23 17:07:12 +0200

    render conditionaly

--- src/components/accordion/accordion.component.jsx
+++ src/components/accordion/accordion.component.jsx
@@ -9,17 +9,24 @@ export const Accordion = ({ items }) => {
 
   return (
     <div className="ui styled accordion">
-      {items.map((item, index) => (
-        <React.Fragment key={item.title}>
-          <div className="title active" onClick={() => handleTitleClick(index)}>
-            <i className="dropdown icon"></i>
-            {item.title}
-          </div>
-          <div className="content active">
-            <p>{item.content}</p>
-          </div>
-        </React.Fragment>
-      ))}
+      {items.map((item, index) => {
+        const active = activeIndex === index ? "active" : ""
+
+        return (
+          <React.Fragment key={item.title}>
+            <div
+              className={`title ${active}`}
+              onClick={() => handleTitleClick(index)}
+            >
+              <i className="dropdown icon"></i>
+              {item.title}
+            </div>
+            <div className={`content ${active}`}>
+              <p>{item.content}</p>
+            </div>
+          </React.Fragment>
+        )
+      })}
       <h1>{activeIndex}</h1>
     </div>
   )
