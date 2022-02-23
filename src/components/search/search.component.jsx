34826c712bbab13627fa8fa9ddb4a3bae7c40823
biggicode <larioncescu@yahoo.com>
2022-02-23 18:27:10 +0200

    add structure to search

--- src/components/search/search.component.jsx
+++ src/components/search/search.component.jsx
@@ -1,5 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 
 export const Search = () => {
-  return <div>this is search</div>
+  const [term, setTerm] = useState("")
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter search term</label>
+          <input
+            value={term}
+            className="input"
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
