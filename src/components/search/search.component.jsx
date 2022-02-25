0fff28b3f7ce105e8cd2b01bc6a605ae984060f4
biggicode <larioncescu@yahoo.com>
2022-02-25 13:42:28 +0200

    make initial search

--- src/components/search/search.component.jsx
+++ src/components/search/search.component.jsx
@@ -20,14 +20,18 @@ export const Search = () => {
       setResults(data.query.search)
     }
 
-    const timeoutId = setTimeout(() => {
-      if (term) {
-        search()
-      }
-    }, 500)
+    if (term && !results.length) {
+      search()
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search()
+        }
+      }, 500)
 
-    return () => {
-      clearTimeout(timeoutId)
+      return () => {
+        clearTimeout(timeoutId)
+      }
     }
   }, [term])
 
