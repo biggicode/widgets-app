eeae1905ab049302d9eedafdacb266979167ac67
biggicode <larioncescu@yahoo.com>
2022-02-26 22:47:20 +0200

    make dropdown close when click out

--- src/components/drop-down/drop-down.component.jsx
+++ src/components/drop-down/drop-down.component.jsx
@@ -1,7 +1,17 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 export const DropDown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      () => {
+        setOpen(false)
+      },
+      { capture: true }
+    )
+  }, [])
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) return null
