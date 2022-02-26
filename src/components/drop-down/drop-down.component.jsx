4a7cddc329142895f2eb2a0d6d4ee11ac11ff2fe
biggicode <larioncescu@yahoo.com>
2022-02-27 00:06:36 +0200

    even clean up

--- src/components/drop-down/drop-down.component.jsx
+++ src/components/drop-down/drop-down.component.jsx
@@ -5,17 +5,19 @@ export const DropDown = ({ options, selected, onSelectedChange }) => {
   const ref = useRef()
 
   useEffect(() => {
-    document.body.addEventListener(
-      "click",
-      (event) => {
-        if (ref.current.contains(event.target)) {
-          return
-        }
+    const onBodyClick = (event) => {
+      if (ref.current && ref.current.contains(event.target)) {
+        return
+      }
 
-        setOpen(false)
-      },
-      { capture: true }
-    )
+      setOpen(false)
+    }
+
+    document.body.addEventListener("click", onBodyClick)
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick)
+    }
   }, [])
 
   const renderedOptions = options.map((option) => {
