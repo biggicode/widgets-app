073a56119a2f1250190aeac72d644f6401e3d851
biggicode <larioncescu@yahoo.com>
2022-03-03 14:41:42 +0200

    add event listener to links

--- src/components/link/link.component.jsx
+++ src/components/link/link.component.jsx
@@ -4,6 +4,9 @@ export const Link = ({ className, href, children }) => {
   const onLinkClick = (e) => {
     e.preventDefault()
     window.history.pushState({}, "", href)
+
+    const navEvent = new PopStateEvent("popstate")
+    window.dispatchEvent(navEvent)
   }
 
   return (
--- src/components/route/route.component.jsx
+++ src/components/route/route.component.jsx
@@ -1,5 +1,15 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 export const Route = ({ path, children }) => {
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log("location changed")
+    }
+
+    window.addEventListener("popstate", onLocationChange)
+
+    return window.removeEventListener("popstate", onLocationChange)
+  }, [])
+
   return window.location.pathname === path ? children : null
 }
